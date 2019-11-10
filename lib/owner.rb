5daea6559e3c754693ab6e536aee23c46980a5f8
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 18:19:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
 class Owner
-  # code goes here
+   attr_reader :name, :species
+   @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
+  
+  def species
+    @species = "human" 
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    all_cats = Cat.all.select {|cat| cat.owner == self}
+    all_cats
+  end
+  
+  def dogs
+    all_dogs = Dog.all.select {|dog| dog.owner == self}
+    all_dogs
+  end
+  
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+  end
+  
+  def walk_dogs 
+    
+  end 
+  
+  def feed_cat 
+    
+  end 
 end
